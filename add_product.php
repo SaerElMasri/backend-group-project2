4224e35b01fb95eeb80d6c54022dc15c8de7b8f4
aliynajjar <ali.y.alnajjar@gmail.com>
2023-03-05 19:26:01 +0200

    Added add product API

---
+++ add_product.php
@@ -0,0 +1,22 @@
+<?php
+
+include('connect.php');
+
+$name = $_POST['name'];
+$description = $_POST['description'];
+$price = $_POST['price'];
+$category = $_POST['category'];
+$image_url = $_POST['image_url'];
+
+$query =  $conn->prepare('insert into products(name,description,price,category,image_url) values(?,?,?,?,?)');
+$query->bind_param('ssiss',$name, $description, $price, $category,$image_url);
+$query->execute();
+$response['status'] = "Product is successfully added!";
+$response['name'] = $name;
+$response['description'] = $description;
+$response['price'] = $price;
+$response['category'] = $category;
+$response['image_url'] = $image_url;
+
+
+echo json_encode($response);
