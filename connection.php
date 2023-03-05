f74cb4d95011686dd564c05e4d91ef16d77c7e99
aliynajjar <ali.y.alnajjar@gmail.com>
2023-03-05 18:27:53 +0200

    Added edit profile API

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$db_host="localhost";
+$db_user ="root";
+$db_pass = null;
+$db_name = "ecommerce_projectdb";
+$mysqli = new mysqli($db_host,$db_user,$db_pass,$db_name);
+
+?>
---
+++ edit_profile.php
@@ -0,0 +1,33 @@
+<?php
+include('connect.php');
+
+$id = $_POST["id"];
+$first_name = $_POST["first_name"];
+$last_name = $_POST["last_name"];
+$email = $_POST["email"];
+
+$query = $conn->prepare('SELECT * FROM users WHERE id = ?');
+$query -> bind_param('i', $id);
+$query -> execute();
+$result = $query -> get_result();
+$row = $result -> fetch_assoc();
+
+$response = [];
+if(!empty($row)){
+    $sql = "UPDATE users set first_name =?,last_name=?,email=? WHERE id=?";
+    $statement = $conn->prepare($sql);
+    $statement->bind_param('sssi', $first_name,$last_name,$email,$id);
+    $statement->execute();
+    $response['status'] = "Profile Changed";
+    $response['user_id'] = $id;
+    $response['first_name'] = $first_name;
+    $response['last_name'] = $last_name;
+    $response['$email'] = $email;
+
+} else{
+    $response['status'] = "Email does not exist";
+}
+echo json_encode($response);
+
+
+?>
